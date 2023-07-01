b50c4576e0d41e77e5da657e9c034c7681cf7b53
Michel Muniagiseny <mafri.michel@gmail.com>
2023-07-01 19:36:04 +0300

    SCR-4 Fix path for prod

---
+++ src/main.js
@@ -0,0 +1,68 @@
+import './styles/style.scss'
+
+let itemsLeft = gsap.utils.toArray('.gallery__left .gallery__item'),
+    itemsRight = gsap.utils.toArray('.gallery__right .gallery__item')
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+if (ScrollTrigger.isTouch !== 1) {
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,
+    effects: true
+  })
+
+  gsap.fromTo('.hero-section',
+    {
+      opacity: 1
+    },
+    {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: '.hero-section',
+        start: 'center',
+        end: '800',
+        scrub: true
+    }
+  })
+
+  itemsLeft.forEach(item => {
+    gsap.fromTo(item,
+      {
+        x: -500,
+        opacity: 0
+      },
+      {
+        x: 0,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: item,
+          start: '-850',
+				  end: '-100',
+          scrub: true
+        }
+      }
+    )
+  })
+
+  itemsRight.forEach(item => {
+    gsap.fromTo(item,
+      {
+        opacity: 0,
+        x: 500
+      },
+      {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '0',
+          end: 'top',
+          scrub: true
+			  }
+		  }
+    )
+  })
+}
+
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite"
 import pugPlugin from "vite-plugin-pug"
+import copy from 'rollup-plugin-copy'
 import content from './src/content.json'
 
 const options = { pretty: true } // FIXME: pug pretty is deprecated!
 const locals = { data: content }
 
 export default defineConfig({
-  plugins: [pugPlugin(options, locals)],
+  plugins: [
+    pugPlugin(options, locals),
+    copy({
+      targets: [
+        { src: './src/img/**/*', dest: 'dist/src/img/' },
+      ],
+      hook: 'writeBundle',
+    })
+  ]
 })
